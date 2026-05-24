fedd044020616eb139e0eb039a0433090631749a
BantaDigital <mark@bantadigitalmarketing.com>
2026-05-25 08:40:31 +0930

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://egsednfetxvhzptiptek.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnc2VkbmZldHh2aHpwdGlwdGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NDczNjIsImV4cCI6MjA5NTIyMzM2Mn0.TqjwqxchRmhcybiEf-XPLXp7IT7l-kZYcPGaMGU8kHc';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
